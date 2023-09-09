b2b809dde10c393cf4ab568aa0e898626f651496
Herman Groenenboom <taartaar@gmail.com>
2023-09-09 13:57:08 +0200

    Reverted RepeatMover script to working state

--- Assets/Scripts/Level/RepeatMover.cs
+++ Assets/Scripts/Level/RepeatMover.cs
@@ -5,23 +5,11 @@ public class RepeatMover : MonoBehaviour
     public float start = 80f;
     public float distance = 100f;
     public float duration = 5f;
-    public float speed = 5f;
-
-    float Speed => distance/duration*PlayerController.Instance.speed;
-
-    private void Start()
-    {
-        Vector3 pos = transform.position;
-        pos.z = start;
-        transform.position = pos;
-    }
 
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.z -= Speed*Time.deltaTime;
-
-        pos.z = pos.z % (distance - start);
+        var pos = transform.position;
+        pos.z = start - Time.time/duration % 1f * distance;
         transform.position = pos;
     }
 }
