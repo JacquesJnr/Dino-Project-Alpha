61e8610c300006f3ff4b9d7a29707fc08243f189
Jacques Visser <jacquesvisserjnr@gmail.com>
2023-09-13 17:38:18 +0400

    Created Particles For Running

---
+++ Assets/Scripts/VFX/RunParticles.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunParticles : MonoBehaviour
+{
+    public Transform particleParent;
+    public ParticleSystem runFX;
+}
